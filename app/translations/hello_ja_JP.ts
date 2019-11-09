21dcddfdc0cb163a44895c693f112c1c349b1d38
enjoysoftware <username@users.noreply.github.com>
2019-11-09 12:00:15 +0900

    Create translations

---
+++ app/translations/hello_ja_JP.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="14"/>
+        <source>Please click the button.</source>
+        <translation>ボタンをクリックしてください。</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="27"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;Click OK or Cancel&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;↓&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;OKかキャンセルをクリックしてくだい。&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;↓&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="16"/>
+        <source>accenpted</source>
+        <translation>承諾された</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="16"/>
+        <source>acccenpted.</source>
+        <translation>承諾された。</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="20"/>
+        <source>rejected</source>
+        <translation>拒否された</translation>
+    </message>
+    <message>
+        <location filename="../dialog.cpp" line="20"/>
+        <source>rejected.</source>
+        <translation>拒否された。</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>HelloWorld GUI</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Click me!</source>
+        <translation>クリックしてください！</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="21"/>
+        <source>Do you want to exit?</source>
+        <translation>終了してもよろしいですか？</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="21"/>
+        <source>This program will closed.
+Are you OK?</source>
+        <translation>このプログラムは閉じられます。
+OKですか？</translation>
+    </message>
+</context>
+</TS>
