8dbf7d185a3a8afbd57051b956cd5bbf57fb4f72
enjoysoftware <username@users.noreply.github.com>
2019-12-12 17:32:02 +0900

    不安定であった翻訳を更新

--- app/translations/hello_ja_JP.ts
+++ app/translations/hello_ja_JP.ts
@@ -2,51 +2,17 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
 <context>
-    <name>Dialog</name>
-    <message>
-        <source>Please click the button.</source>
-        <translation type="vanished">ボタンをクリックしてください。</translation>
-    </message>
-    <message>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;Click OK or Cancel&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;↓&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;OKかキャンセルをクリックしてくだい。&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-size:24pt;&quot;&gt;↓&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <source>accenpted</source>
-        <translation type="vanished">承諾された</translation>
-    </message>
-    <message>
-        <source>acccenpted.</source>
-        <translation type="vanished">承諾された。</translation>
-    </message>
-    <message>
-        <source>accepted</source>
-        <translation type="vanished">受けいられれました</translation>
-    </message>
-    <message>
-        <source>accepted.</source>
-        <translation type="vanished">受け入れられました。</translation>
-    </message>
-    <message>
-        <source>rejected</source>
-        <translation type="vanished">拒否された</translation>
-    </message>
-    <message>
-        <source>rejected.</source>
-        <translation type="vanished">拒否された。</translation>
-    </message>
-</context>
-<context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="20"/>
         <source>HelloWorld GUI</source>
-        <translation></translation>
+        <translation>HelloWorld GUI</translation>
     </message>
     <message>
+        <location filename="../mainwindow.ui" line="53"/>
         <source>Print a friendly, customizable greeting.
 </source>
-        <translation type="vanished">フレンドリーでカスタマイズ可能なあいさつを表示します。
+        <translation>フレンドリーでカスタマイズ可能なあいさつを表示します。
 </translation>
     </message>
     <message>
@@ -61,6 +27,7 @@
     </message>
     <message>
         <location filename="../mainwindow.ui" line="80"/>
+        <location filename="../mainwindow.cpp" line="18"/>
         <source>Greeting</source>
         <translation>あいさつ</translation>
     </message>
@@ -70,27 +37,9 @@
         <translation>バージョン</translation>
     </message>
     <message>
-        <source>Click to open the dialog.</source>
-        <translation type="vanished">クリックしてダイアログを開きます。</translation>
-    </message>
-    <message>
-        <source>Click me!</source>
-        <translation type="vanished">クリックしてください！</translation>
-    </message>
-    <message>
-        <source>Do you want to exit?</source>
-        <translation type="vanished">終了してもよろしいですか？</translation>
-    </message>
-    <message>
-        <source>This program will closed.
-Are you OK?</source>
-        <translation type="vanished">このプログラムは閉じられます。
-OKですか？</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.cpp" line="9"/>
         <source>Version %1</source>
-        <translation>バージョン %1</translation>
+        <translation type="unfinished">バージョン %1</translation>
     </message>
 </context>
 </TS>
