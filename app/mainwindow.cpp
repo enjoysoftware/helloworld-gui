b70d391c344ede832271fd937151471e8650d2ff
enjoysoftware <username@users.noreply.github.com>
2020-03-04 10:21:22 +0900

    Set to move to center automatically

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDesktopWidget>
 #define VERSION QString("1.3.1")
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -38,8 +39,10 @@ void MainWindow::showAbout(){
 }
 void MainWindow::loadSettings(){
     QRect def = geometry();
-    int x = settings->value("window_x",def.x()).toInt();
-    int y = settings->value("window_y",def.y()).toInt();
+    QDesktopWidget *deskwidget = QApplication::desktop();
+    QRect desksize = deskwidget->screenGeometry();
+    int x = settings->value("window_x",(desksize.height()-height())/2).toInt();
+    int y = settings->value("window_y",(desksize.width()-height())/2).toInt();
     int w = settings->value("window_width",def.width()).toInt();
     int h = settings->value("window_height",def.height()).toInt();
     setGeometry(x,y,w,h);
