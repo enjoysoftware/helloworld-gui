3181c1c941c04c7b797bd92ee650eca3db7ba195
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-04-01 16:22:35 +0900

    Update Korean translations

--- app/translations/hello_ko.ts
+++ app/translations/hello_ko.ts
@@ -9,19 +9,34 @@
         <translation>HelloWorld GUI</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="106"/>
+        <location filename="../mainwindow.ui" line="27"/>
         <source>Print a friendly, customizable greeting.
 </source>
         <translation>반가운 인사말을 (인사말은 설정할 수 있음) 출력합니다.
 </translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="120"/>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Where to display</source>
+        <translation>표시 할 곳</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="41"/>
+        <source>Messagebox</source>
+        <translation>메세지 박스</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="48"/>
+        <source>In this window</source>
+        <translation>이 창에서</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
         <source>&amp;About</source>
         <translation>이에 대해 (&amp;A)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="97"/>
+        <location filename="../mainwindow.ui" line="143"/>
         <source>&amp;Display</source>
         <translation>표시 (&amp;D)</translation>
     </message>
@@ -30,12 +45,12 @@
         <translation type="vanished">보기</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="84"/>
+        <location filename="../mainwindow.ui" line="130"/>
         <source>Hello,world!</source>
         <translation>안녕하세요, 여러분!</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="55"/>
+        <location filename="../mainwindow.ui" line="101"/>
         <source>Greeting message</source>
         <translation>인사말</translation>
     </message>
@@ -48,14 +63,29 @@
         <translation type="vanished">버전 %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="21"/>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <location filename="../mainwindow.cpp" line="26"/>
         <source>Greeting</source>
         <translation>인사말</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="36"/>
+        <location filename="../mainwindow.cpp" line="29"/>
+        <source>No where to display greeting message specified</source>
+        <translation>인사말 메시지를 표시 할 위치가 없습니다</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="29"/>
+        <source>No where to display greeting message specified.Please specify where to display greeting message.</source>
+        <translation>인사말 메시지를 표시 할 위치를 지정하지 않았습니다. 인사말 메시지를 표시 할 위치를 지정하십시오.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br&gt;%3&lt;br&gt;License:&lt;b&gt;%4&lt;/b&gt;</source>
+        <translation>&lt;h2&gt;About&lt;/h2&gt;Qt 기반의이 응용 프로그램은 GUI에 친숙하고 사용자 정의 가능한 인사말을 표시합니다. &lt;br&gt; &lt;i&gt; 버전 %1 &lt;/i&gt; &lt;br&gt; 리포지토리 : &lt;a href=&quot;%2&quot;&gt; %2 &lt;/a&gt; &lt;br &gt; %3 &lt;br&gt; 라이센스 : &lt;b&gt; %4 &lt;/b&gt;</translation>
+    </message>
+    <message>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt;https://github.com/enjoysoftware/helloworld-gui&lt;/a&gt;&lt;br&gt;(C) 2020 EnjoySoftware&lt;br&gt;License:&lt;b&gt;GPL-3&lt;/b&gt;</source>
-        <translation>&lt;h2&gt;이소프트웨어에 대한 &lt;/h2&gt; Qt 기반이 응용 프로그램은 친절하고 맞춤형 인사를 GUI에 표시합니다. &lt;br&gt; &lt;i&gt; 버전 %1 &lt;/i&gt; &lt;br&gt; 저장소 : &lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt; https://github.com/enjoysoftware/helloworld- gui &lt;/a&gt; &lt;br&gt; (C) 2020 EnjoySoftware&lt;br&gt;라이센스 :&lt;b&gt;GPL-3&lt;/b&gt;</translation>
+        <translation type="vanished">&lt;h2&gt;이소프트웨어에 대한 &lt;/h2&gt; Qt 기반이 응용 프로그램은 친절하고 맞춤형 인사를 GUI에 표시합니다. &lt;br&gt; &lt;i&gt; 버전 %1 &lt;/i&gt; &lt;br&gt; 저장소 : &lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt; https://github.com/enjoysoftware/helloworld- gui &lt;/a&gt; &lt;br&gt; (C) 2020 EnjoySoftware&lt;br&gt;라이센스 :&lt;b&gt;GPL-3&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt;https://github.com/enjoysoftware/helloworld-gui&lt;/a&gt;&lt;br&gt;(C) 2020 EnjoySoftware</source>
