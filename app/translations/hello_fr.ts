a29b8f9a3c1ba17943c3ddf0058fcb3fc088dda3
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-04-01 16:17:07 +0900

    Update French translations

--- app/translations/hello_fr.ts
+++ app/translations/hello_fr.ts
@@ -9,41 +9,71 @@
         <translation>HelloWorld GUI</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="55"/>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Where to display</source>
+        <translation>Où afficher</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="41"/>
+        <source>Messagebox</source>
+        <translation>Boîte de message</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="48"/>
+        <source>In this window</source>
+        <translation>Dans cette fenêtre</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="101"/>
         <source>Greeting message</source>
         <translation>Message d&apos;accueil</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="84"/>
+        <location filename="../mainwindow.ui" line="130"/>
         <source>Hello,world!</source>
         <translation>Bonjour le monde!</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="97"/>
+        <location filename="../mainwindow.ui" line="143"/>
         <source>&amp;Display</source>
         <translation>&amp;Affichage</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="106"/>
+        <location filename="../mainwindow.ui" line="27"/>
         <source>Print a friendly, customizable greeting.
 </source>
         <translation>Imprimez un message d&apos;accueil convivial et personnalisable.
 </translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="120"/>
+        <location filename="../mainwindow.ui" line="64"/>
         <source>&amp;About</source>
         <translation>&amp;About</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="21"/>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <location filename="../mainwindow.cpp" line="26"/>
         <source>Greeting</source>
         <translation>Salutation</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="36"/>
+        <location filename="../mainwindow.cpp" line="29"/>
+        <source>No where to display greeting message specified</source>
+        <translation>Vous n&apos;avez pas spécifié où afficher le message d&apos;accueil</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="29"/>
+        <source>No where to display greeting message specified.Please specify where to display greeting message.</source>
+        <translation>Aucun endroit où afficher le message d&apos;accueil spécifié. Veuillez spécifier où afficher le message d&apos;accueil.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br&gt;%3&lt;br&gt;License:&lt;b&gt;%4&lt;/b&gt;</source>
+        <translation>&lt;h2&gt; About&lt;/h2&gt; basée sur Qt, cette application affiche un message d&apos;accueil convivial et personnalisable dans l&apos;interface graphique. &lt;br&gt; &lt;i&gt; Version %1 &lt;/i&gt; &lt;br&gt; Référentiel: &lt;a href=&quot;%2&quot;&gt; %2 &lt;/a&gt; &lt;br&gt;%3 &lt;br&gt; Licence: &lt;b&gt;%4 &lt;/b&gt;</translation>
+    </message>
+    <message>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt;https://github.com/enjoysoftware/helloworld-gui&lt;/a&gt;&lt;br&gt;(C) 2020 EnjoySoftware&lt;br&gt;License:&lt;b&gt;GPL-3&lt;/b&gt;</source>
-        <translation>&lt;h2&gt;About&lt;/h2&gt;basée sur Qt, cette application affiche un message d&apos;accueil convivial et personnalisable dans l&apos;interface graphique.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Référentiel:&lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt;https://github.com/enjoysoftware/helloworld-gui&lt;/a&gt;&lt;br&gt;(C) 2020 EnjoySoftware&lt;br&gt;License:&lt;b&gt;GPL-3&lt;/b&gt;</translation>
+        <translation type="vanished">&lt;h2&gt;About&lt;/h2&gt;basée sur Qt, cette application affiche un message d&apos;accueil convivial et personnalisable dans l&apos;interface graphique.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Référentiel:&lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt;https://github.com/enjoysoftware/helloworld-gui&lt;/a&gt;&lt;br&gt;(C) 2020 EnjoySoftware&lt;br&gt;License:&lt;b&gt;GPL-3&lt;/b&gt;</translation>
     </message>
     <message>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;https://github.com/enjoysoftware/helloworld-gui&quot;&gt;https://github.com/enjoysoftware/helloworld-gui&lt;/a&gt;&lt;br&gt;(C) 2020 EnjoySoftware</source>
