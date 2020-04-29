44d063c1280bb340430815825d800a47d7ce06fc
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-04-29 17:58:18 +0900

    Update Japanese translations

--- app/translations/hello_ja_JP.ts
+++ app/translations/hello_ja_JP.ts
@@ -9,39 +9,44 @@
         <translation>HelloWorld GUI</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="27"/>
+        <location filename="../mainwindow.ui" line="31"/>
         <source>Print a friendly, customizable greeting.
 </source>
         <translation>フレンドリーでカスタマイズ可能なあいさつを表示します。
 </translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="35"/>
+        <location filename="../mainwindow.ui" line="39"/>
         <source>Where to display</source>
         <translation>表示する場所</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="41"/>
+        <location filename="../mainwindow.ui" line="45"/>
         <source>Messagebox</source>
         <translation>メッセージボックス</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="48"/>
+        <location filename="../mainwindow.ui" line="52"/>
         <source>In this window</source>
         <translation>このウィンドウの中</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="64"/>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Desktop notifications</source>
+        <translation>デスクトップ通知</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="75"/>
         <source>&amp;About</source>
         <translation>これについて(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="130"/>
+        <location filename="../mainwindow.ui" line="141"/>
         <source>Hello,world!</source>
         <translation>世界よ、こんにちは！</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="143"/>
+        <location filename="../mainwindow.ui" line="154"/>
         <source>&amp;Display</source>
         <translation>表示(&amp;D)</translation>
     </message>
@@ -50,7 +55,7 @@
         <translation type="vanished">世界よ、こんにちは</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="101"/>
+        <location filename="../mainwindow.ui" line="112"/>
         <source>Greeting message</source>
         <translation>あいさつメッセージ</translation>
     </message>
@@ -59,23 +64,29 @@
         <translation type="vanished">表示</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="25"/>
-        <location filename="../mainwindow.cpp" line="26"/>
+        <location filename="../mainwindow.cpp" line="27"/>
+        <source>&amp;Quit</source>
+        <translation>終了(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <location filename="../mainwindow.cpp" line="46"/>
         <source>Greeting</source>
         <translation>あいさつ</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="29"/>
+        <location filename="../mainwindow.cpp" line="49"/>
         <source>No where to display greeting message specified</source>
         <translation>あいさつメッセージを表示する場所が指定されていません</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="29"/>
+        <location filename="../mainwindow.cpp" line="49"/>
         <source>No where to display greeting message specified.Please specify where to display greeting message.</source>
         <translation>あいさつメッセージを表示する場所が指定されていません。あいさつメッセージを表示する場所を指定してください。</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="45"/>
+        <location filename="../mainwindow.cpp" line="66"/>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br&gt;%3&lt;br&gt;License:&lt;b&gt;%4&lt;/b&gt;</source>
         <translation>&lt;h2&gt;このソフトウェアについて&lt;/h2&gt;Qtベースのこのアプリケーションは、GUIの中にフレンドリーでカスタマイズ可能なあいさつを表示します。&lt;br&gt;&lt;i&gt;バージョン %1&lt;/i&gt;&lt;br&gt;リポジトリ:&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br&gt;%3&lt;br&gt;ライセンス:&lt;b&gt;%4&lt;/b&gt;</translation>
     </message>
