435e3275488cea627c55ede814154b387c81e323
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-04-29 17:58:29 +0900

    Update TS files

--- app/translations/hello_fr.ts
+++ app/translations/hello_fr.ts
@@ -9,65 +9,76 @@
         <translation>HelloWorld GUI</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="35"/>
+        <location filename="../mainwindow.ui" line="39"/>
         <source>Where to display</source>
         <translation>Où afficher</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="41"/>
+        <location filename="../mainwindow.ui" line="45"/>
         <source>Messagebox</source>
         <translation>Boîte de message</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="48"/>
+        <location filename="../mainwindow.ui" line="52"/>
         <source>In this window</source>
         <translation>Dans cette fenêtre</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="101"/>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Desktop notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="112"/>
         <source>Greeting message</source>
         <translation>Message d&apos;accueil</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="130"/>
+        <location filename="../mainwindow.ui" line="141"/>
         <source>Hello,world!</source>
         <translation>Bonjour le monde!</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="143"/>
+        <location filename="../mainwindow.ui" line="154"/>
         <source>&amp;Display</source>
         <translation>&amp;Affichage</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="27"/>
+        <location filename="../mainwindow.ui" line="31"/>
         <source>Print a friendly, customizable greeting.
 </source>
         <translation>Imprimez un message d&apos;accueil convivial et personnalisable.
 </translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="64"/>
+        <location filename="../mainwindow.ui" line="75"/>
         <source>&amp;About</source>
         <translation>&amp;About</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="25"/>
-        <location filename="../mainwindow.cpp" line="26"/>
+        <location filename="../mainwindow.cpp" line="27"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <location filename="../mainwindow.cpp" line="46"/>
         <source>Greeting</source>
         <translation>Salutation</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="29"/>
+        <location filename="../mainwindow.cpp" line="49"/>
         <source>No where to display greeting message specified</source>
         <translation>Vous n&apos;avez pas spécifié où afficher le message d&apos;accueil</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="29"/>
+        <location filename="../mainwindow.cpp" line="49"/>
         <source>No where to display greeting message specified.Please specify where to display greeting message.</source>
         <translation>Aucun endroit où afficher le message d&apos;accueil spécifié. Veuillez spécifier où afficher le message d&apos;accueil.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="45"/>
+        <location filename="../mainwindow.cpp" line="66"/>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br&gt;%3&lt;br&gt;License:&lt;b&gt;%4&lt;/b&gt;</source>
         <translation>&lt;h2&gt; About&lt;/h2&gt; basée sur Qt, cette application affiche un message d&apos;accueil convivial et personnalisable dans l&apos;interface graphique. &lt;br&gt; &lt;i&gt; Version %1 &lt;/i&gt; &lt;br&gt; Référentiel: &lt;a href=&quot;%2&quot;&gt; %2 &lt;/a&gt; &lt;br&gt;%3 &lt;br&gt; Licence: &lt;b&gt;%4 &lt;/b&gt;</translation>
     </message>
--- app/translations/hello_ko.ts
+++ app/translations/hello_ko.ts
@@ -9,34 +9,39 @@
         <translation>HelloWorld GUI</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="27"/>
+        <location filename="../mainwindow.ui" line="31"/>
         <source>Print a friendly, customizable greeting.
 </source>
         <translation>반가운 인사말을 (인사말은 설정할 수 있음) 출력합니다.
 </translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="35"/>
+        <location filename="../mainwindow.ui" line="39"/>
         <source>Where to display</source>
         <translation>표시 할 곳</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="41"/>
+        <location filename="../mainwindow.ui" line="45"/>
         <source>Messagebox</source>
         <translation>메세지 박스</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="48"/>
+        <location filename="../mainwindow.ui" line="52"/>
         <source>In this window</source>
         <translation>이 창에서</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="64"/>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Desktop notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="75"/>
         <source>&amp;About</source>
         <translation>이에 대해 (&amp;A)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="143"/>
+        <location filename="../mainwindow.ui" line="154"/>
         <source>&amp;Display</source>
         <translation>표시 (&amp;D)</translation>
     </message>
@@ -45,12 +50,12 @@
         <translation type="vanished">보기</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="130"/>
+        <location filename="../mainwindow.ui" line="141"/>
         <source>Hello,world!</source>
         <translation>안녕하세요, 여러분!</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="101"/>
+        <location filename="../mainwindow.ui" line="112"/>
         <source>Greeting message</source>
         <translation>인사말</translation>
     </message>
@@ -63,23 +68,29 @@
         <translation type="vanished">버전 %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="25"/>
-        <location filename="../mainwindow.cpp" line="26"/>
+        <location filename="../mainwindow.cpp" line="27"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <location filename="../mainwindow.cpp" line="46"/>
         <source>Greeting</source>
         <translation>인사말</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="29"/>
+        <location filename="../mainwindow.cpp" line="49"/>
         <source>No where to display greeting message specified</source>
         <translation>인사말 메시지를 표시 할 위치가 없습니다</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="29"/>
+        <location filename="../mainwindow.cpp" line="49"/>
         <source>No where to display greeting message specified.Please specify where to display greeting message.</source>
         <translation>인사말 메시지를 표시 할 위치를 지정하지 않았습니다. 인사말 메시지를 표시 할 위치를 지정하십시오.</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="45"/>
+        <location filename="../mainwindow.cpp" line="66"/>
         <source>&lt;h2&gt;About&lt;/h2&gt;Qt-based, this application displays a friendly, customizable greeting in GUI.&lt;br&gt;&lt;i&gt;Version %1&lt;/i&gt;&lt;br&gt;Repository:&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt;&lt;br&gt;%3&lt;br&gt;License:&lt;b&gt;%4&lt;/b&gt;</source>
         <translation>&lt;h2&gt;About&lt;/h2&gt;Qt 기반의이 응용 프로그램은 GUI에 친숙하고 사용자 정의 가능한 인사말을 표시합니다. &lt;br&gt; &lt;i&gt; 버전 %1 &lt;/i&gt; &lt;br&gt; 리포지토리 : &lt;a href=&quot;%2&quot;&gt; %2 &lt;/a&gt; &lt;br &gt; %3 &lt;br&gt; 라이센스 : &lt;b&gt; %4 &lt;/b&gt;</translation>
     </message>
