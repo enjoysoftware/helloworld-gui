3846aab50836aff7c279ed7d730e1eaf4352d6e5
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-08-10 18:04:35 +0900

    Revert "Auto deploy"
    This reverts commit 0566a6050f3b956fd308ddcec2fa4fc16a995040.

--- scripts/packaging.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir helloworld-gui-${TRAVIS_TAG}
-cp app/helloworld-gui helloworld-gui-${TRAVIS_TAG}
-bash scripts/copy-deps.sh helloworld-gui-${TRAVIS_TAG}/helloworld-gui helloworld-gui-${TRAVIS_TAG}
-tar czvf helloworld-gui-${TRAVIS_TAG}_${TRAVIS_CPU_ARCH}.tar.gz helloworld-gui-${TRAVIS_TAG}
